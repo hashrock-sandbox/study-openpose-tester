6ac0ad10c0cd44f909faa17d8728117d955c8a63
hashrock <hashedrock@gmail.com>
2017-06-11 10:27:41 +0000

    update after meetup

--- index.py
+++ index.py
@@ -1,22 +1,35 @@
 import os
 import uuid
 
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, send_file
+from flask_cors import CORS, cross_origin
+import subprocess
 
-UPLOAD_FOLDER = './uploads'
+os.chdir('../openpose')
+openpose_bin = './build/examples/openpose/openpose.bin'
+input_dir = "../openpose-web/input"
+output_dir = "../openpose-web/output"
+
+
+def process():
+    output = subprocess.check_output([openpose_bin, "--image_dir", input_dir, "--write_images", output_dir, "--write_keypoint_json", output_dir, "--no_display", "--face"])
+    print output
+
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin()
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             return "No file part"
         file = request.files['file']
@@ -24,8 +37,11 @@ def upload_file():
             return "No selected file"
         if file and allowed_file(file.filename):
             filename = str(uuid.uuid1())
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return filename
+            in_file = os.path.join(input_dir, filename + ".png") 
+            file.save(in_file)
+	    process()
+            os.remove(in_file)
+            return send_file(os.path.join(output_dir, filename+"_rendered.png"))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -34,5 +50,28 @@ def upload_file():
       <p><input type=file name=file>
          <input type=submit value=Upload>
     </form>
+    <h2>JSON</h2>
+    <form action="/json" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+ 
     '''
+
+@app.route("/json", methods=["POST"])
+@cross_origin()
+def get_json():
+    file = request.files['file']
+    filename = str(uuid.uuid1())
+    in_file = os.path.join(input_dir, filename + ".png") 
+    file.save(in_file)
+    process()
+    os.remove(in_file)
+    return send_file(os.path.join(output_dir, filename + "_pose.json"))
+
+
+
+if __name__ == "__main__":
+    app.run(port=80, host="0.0.0.0")
+
 
